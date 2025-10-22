665d2865b472f95a4115501030a6c70dd508bd98
daverave1212 <crazyman_dave2000@yahoo.com>
2025-10-22 18:13:51 +0300

    Added open windows in the taskbar.

--- drug-dealer-chat-simulator/src/components-standalone/os/OSTaskBar/OSTaskBar.js
+++ drug-dealer-chat-simulator/src/components-standalone/os/OSTaskBar/OSTaskBar.js
@@ -4,6 +4,8 @@ import '../Styles/OSStyle.css'
 import { ALL_APPS } from '../../../services/app-manager'
 import { useContext } from 'react'
 import StartContextMenuContext from '../../../global-state/StartContextMenuContext'
+import { useGameCalendar, useGameClock, useUser } from '../../../global-state/AppData'
+import { OpenAppsContext } from '../../../global-state/OpenAppsContext'
 
 function StartMenu() {
 
@@ -24,11 +26,56 @@ function StartMenu() {
     )
 }
 
+function TaskBarInfo() {
+
+    const [{ hours, minutes }] = useGameClock()
+    const [{ year, month, day, weekday }] = useGameCalendar()
+
+    const truncatedHour = hours < 10? '0' + hours: hours
+
+    return <div className='info flex'>
+        <div className='calendar'>
+            {weekday} {truncatedHour}:{minutes}
+        </div>
+        <div className='padding-quarter padding-bottom-0' style={{paddingTop: '0.35rem'}}>
+            <img src="/Icons/Apps/Calendar.png"/>
+        </div>
+        <div className='calendar'>
+            {year} {month} {day}
+        </div>
+    </div>
+
+}
+
+function TaskBarWindows() {
+
+    function TaskBarApp({appConfig}) {
+        return <div className='task-bar-app flex row padding-right-half os-highlight'>
+            <div className='padding-quarter padding-bottom-0' style={{paddingTop: '0.35rem'}}>
+                <img src={`/Icons/Apps/${appConfig.name}.png`}/>
+            </div>
+            <div className='name'>
+                [{appConfig.name}]
+            </div>
+        </div>
+    }
+
+    const { openApps } = useContext(OpenAppsContext)
+
+    return <div className='flex row'>
+        { openApps.map(appConfig => <TaskBarApp appConfig={appConfig}/>) }
+    </div>
+}
+
 export default function OSTaskBar({ children }) {
 
     return (
         <div className="os-task-bar os-background relative unselectable">
-            <StartMenu/>
+            <div className='flex row'>
+                <StartMenu/>
+                <TaskBarWindows/>
+            </div>
+            <TaskBarInfo/>
         </div>
     )
 }
